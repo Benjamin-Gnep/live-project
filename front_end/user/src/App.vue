08e48d147096729f1e045dd41974fed757539abb
RadishBear <1798164846@qq.com>
2020-03-15 13:19:12 +0800

    新建用户界面，构建了建议表单和名片

---
+++ front_end/user/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <VisitingCard id='visitingCard'></VisitingCard>
+    <Form></Form>
+    <el-button>结束预约</el-button>
+    <el-button>发起新预约</el-button>
+  </div>
+</template>
+
+<script>
+  import Form from './components/Form'
+  import VisitingCard from './components/VisitingCard'
+  import Select from './components/Select'
+export default {
+  name: 'App',
+  components: {
+    Form,
+    Select,
+    VisitingCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#visitingCard{
+  margin: 0 auto;
+}
+</style>
---
+++ front_end/user/src/components/Form.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app" style="width: 500px;margin: 0 auto">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="姓名" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="身份证" prop="id">
+        <el-input v-model="ruleForm.id"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="ruleForm.phone"></el-input>
+      </el-form-item>
+      <el-form-item label="预约数量" prop="number">
+        <el-input v-model="ruleForm.number"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">立即预约</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>export default {
+    name: 'Form',
+  data() {
+    return {
+      ruleForm: {
+        name: '',
+        id: '',
+        phone: '',
+        number: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' },
+          { min: 1, message: '请输入名字', trigger: 'blur' }
+        ],
+        id: [
+          { required: true, message: '请输入身份证号', trigger: 'blur' },
+          { min: 15, max: 18, message: '身份证号位数不对', trigger: 'blur' }
+        ],
+        phone: [
+           { required: true, message: '请输入手机号', trigger: 'blur' }
+        ],
+        number: [
+           { required: true, message: '请输入预购数量', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert(this.ruleForm['name'])
+          alert()
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+  }
+</script>
+
+<style scoped>
+
+</style>
