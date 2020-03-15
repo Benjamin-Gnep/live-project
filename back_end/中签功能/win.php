841ab640cd11bc5a8a66f4a5127fec0137389aaa
129JP <61146413+129JP@users.noreply.github.com>
2020-03-15 13:29:32 +0800

    Create win.php

---
+++ back_end/中签功能/win.php
@@ -0,0 +1,22 @@
+<?php
+	header('Content-type: text/html; charset=UTF8');
+	$user="RemoteUser";
+  	$host='123.57.41.237';
+	$DBpassword='Remote1798164846';
+	$dbName='Mask';
+	$link=new mysqli($host,$user,$DBpassword,$dbName);
+	if ($link->connect_error)
+	{
+		echo ("数据库连接出错\n");
+	}
+	$link->query("SET NAMES utf8");
+	$sql='SELECT * FROM appointment';		// sql查询
+	$res=mysqli_query($link,$sql);
+	$result=[];		// 这个是要发送给前端的数组
+	while($row=$res->fetch_assoc())
+	{
+		array_push($result,$row);
+    }
+    echo json_encode($result);		// 这一步很关键，要把数组打包成json发送
+    $link->close();		// 记得关闭连接
+?>
