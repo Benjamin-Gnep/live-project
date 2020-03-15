db81020f33c70c6185ff061999fdd398ee4a6a9b
129JP <61146413+129JP@users.noreply.github.com>
2020-03-15 15:36:31 +0800

    Update win.php

--- back_end/中签功能/win.php
+++ back_end/中签功能/win.php
@@ -10,13 +10,14 @@
 		echo ("数据库连接出错\n");
 	}
 	$link->query("SET NAMES utf8");
+	$ID=$_GET['id'];
 	$sql='SELECT * FROM appointment';		// sql查询
 	$res=mysqli_query($link,$sql);
 	$result=[];		// 这个是要发送给前端的数组
 	while($row=$res->fetch_assoc())
 	{
 		//array_push($result,$row);
-		if($row['win']=='是')
+		if($row['win']=='是'&&$ID==$row['id'])
 		{
 			//$rows=$row['name']+$row['IDnum']+$row['phone']+$row['num'];
 			array_push($result,$row['name'],$row['IDnum'],$row['phone'],$row['num']);
@@ -25,7 +26,7 @@
 			return $result;
 		}
 	}
-    echo json_encode($result);		
+    echo json_encode($result,"未中签");		
 	$link->close();		
 	return $result;
 ?>
